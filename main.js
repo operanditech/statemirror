e96439b5a3bfd6ef332ef7a4609f187c8368403d
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-29 23:56:42 +0100

    Add websocket handler to main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Observable = require('rxjs')
 const { concatMap } = require('rxjs/operators')
 const ZeroMqReader = require('./readers/zeromq')
 const MongoDbHandler = require('./handlers/mongodb')
+const WebsocketHandler = require('./handlers/websocket')
 
 main().catch(error => {
   console.error(error)
@@ -9,25 +10,26 @@ main().catch(error => {
 })
 
 async function main() {
-  // Instantiate reader and handler
+  // Instantiate reader and handlers
   const reader = new ZeroMqReader()
-  const handler = new MongoDbHandler()
+  const dbHandler = new MongoDbHandler()
+  const wsHandler = new WebsocketHandler()
 
   // Connect them together
   // (using a queue for async operations from concatMap)
   Observable.fromEvent(reader, 'operation')
     .pipe(
       concatMap(async op => {
-        await handler.handle(op)
+        await Promise.all([dbHandler.handle(op), wsHandler.handle(op)])
       })
     )
     .subscribe()
 
-  // Generate a fake stream of operations coming from ZeroMQ
-  reader.generateDummyLoad()
-
-  // Start the handler first
-  await handler.start()
+  // Start the handlers first
+  await Promise.all([dbHandler.start(), wsHandler.start()])
   // And then the reader once everything is ready
   await reader.start()
+
+  // Generate a fake stream of operations coming from ZeroMQ
+  reader.generateDummyLoad()
 }
