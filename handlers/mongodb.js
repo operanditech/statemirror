fbc1867c17d0695cfb544072ce851e5bc26ef598
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-29 18:06:47 +0100

    Add basic MongoDB operation handling

--- handlers/mongodb.js
+++ handlers/mongodb.js
@@ -19,9 +19,30 @@ module.exports = class MongoDbAdaptor {
     this.ready = false
     this.client.close()
   }
-  handle(operation) {
+  async handle(operation) {
     if (this.ready) {
       console.log('Handling operation:', operation)
+
+      const collection = this.db.collection(
+        `${operation.code}_${operation.scope}_${operation.table}`
+      )
+      if (operation.action === 'emplace') {
+        await collection.insertOne({
+          _id: operation.primary_key,
+          ...operation.data
+        })
+      } else if (operation.action === 'modify') {
+        await collection.updateOne(
+          { _id: operation.primary_key },
+          { $set: operation.data }
+        )
+      } else if (operation.action === 'erase') {
+        await collection.deleteOne({ _id: operation.primary_key })
+      } else {
+        throw new TypeError(
+          `Unrecognized operation action type '${operation.action}'`
+        )
+      }
     } else {
       throw new Error('MongoDB adaptor not ready, must call start() first')
     }
