304711a47e6a3e688f1035994a63b5d873fe55b9
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-29 18:01:58 +0100

    Implement operations queue using RxJS

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const DummyReader = require('./readers/dummy')
 const MongoDbHandler = require('./handlers/mongodb')
+const Observable = require('rxjs')
+const { concatMap } = require('rxjs/operators')
 
 main().catch(error => {
   console.error(error)
@@ -10,9 +12,13 @@ async function main() {
   const reader = new DummyReader()
   const handler = new MongoDbHandler()
 
-  reader.on('operation', op => {
-    handler.handle(op)
-  })
+  Observable.fromEvent(reader, 'operation')
+    .pipe(
+      concatMap(async op => {
+        await handler.handle(op)
+      })
+    )
+    .subscribe()
 
   await handler.start()
   await reader.start()
