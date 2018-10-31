566c397f472918604ce31ccf87dae47d9f9206d6
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-31 02:54:54 +0100

    Use new operation format, compound _id, and ensure indexes

--- handlers/mongodb.js
+++ handlers/mongodb.js
@@ -5,6 +5,7 @@ module.exports = class MongoDbHandler {
     this.url = dbUrl
     this.dbName = dbName
     this.ready = false
+    this.collections = {}
   }
   async start() {
     this.client = await MongoClient.connect(
@@ -20,31 +21,42 @@ module.exports = class MongoDbHandler {
     this.client.close()
   }
   async handle(operation) {
-    if (this.ready) {
-      console.log('Handling operation:', operation)
-
-      const collection = this.db.collection(
-        `${operation.code}_${operation.scope}_${operation.table}`
-      )
-      if (operation.action === 'emplace') {
-        await collection.insertOne({
-          _id: operation.primary_key,
-          ...operation.data
-        })
-      } else if (operation.action === 'modify') {
-        await collection.updateOne(
-          { _id: operation.primary_key },
-          { $set: operation.data }
-        )
-      } else if (operation.action === 'erase') {
-        await collection.deleteOne({ _id: operation.primary_key })
-      } else {
-        throw new TypeError(
-          `Unrecognized operation action type '${operation.action}'`
-        )
-      }
-    } else {
+    if (!this.ready) {
       throw new Error('MongoDB handler not ready, must call start() first')
+    }
+    if (['SQUASH', 'COMMIT', 'UNDO'].includes(operation.type)) {
+      // TODO Handle these properly
+      if (this.verbose) {
+        console.log(`Ignoring operation of type ${operation.type}`)
+      }
+      return
+    }
+    console.log('Handling operation:', operation)
+
+    const colName = `${operation.code}_${operation.table}`
+    let collection
+    if (this.collections[colName]) {
+      collection = this.collections[colName]
+    } else {
+      collection = this.collections[colName] = this.db.collection(colName)
+      await collection.createIndex({ '_id.scope': 1, '_id.primary_key': 1 })
+    }
+    if (operation.type === 'EMPLACE') {
+      await collection.insertOne({
+        _id: { primary_key: operation.primary_key, scope: operation.scope },
+        ...operation.data
+      })
+    } else if (operation.type === 'MODIFY') {
+      await collection.replaceOne(
+        { _id: { primary_key: operation.primary_key, scope: operation.scope } },
+        operation.data
+      )
+    } else if (operation.type === 'ERASE') {
+      await collection.deleteOne({
+        _id: { primary_key: operation.primary_key, scope: operation.scope }
+      })
+    } else {
+      throw new TypeError(`Unrecognized operation type '${operation.type}'`)
     }
   }
 }
