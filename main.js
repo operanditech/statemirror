64c9cd2f5718c7de6b9084585b80acec7ceac619
Andres Berrios <andres.berrios.j@gmail.com>
2018-11-01 04:14:27 +0100

    Implement Undoer for handling SQUASH, COMMIT, and UNDO

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { concatMap } = require('rxjs/operators')
 const ZeroMqReader = require('./readers/zeromq')
 const MongoDbHandler = require('./handlers/mongodb')
 const WebsocketHandler = require('./handlers/websocket')
+const Undoer = require('./util/undoer')
 
 main().catch(error => {
   console.error(error)
@@ -14,14 +15,15 @@ async function main() {
   const reader = new ZeroMqReader(undefined, false)
   const dbHandler = new MongoDbHandler()
   const wsHandler = new WebsocketHandler()
+  const undoer = new Undoer(dbHandler)
 
   // Connect them together
   // (using a queue for async operations from concatMap)
   Observable.fromEvent(reader, 'operation')
     .pipe(
-      concatMap(async op => {
-        await Promise.all([dbHandler.handle(op), wsHandler.handle(op)])
-      })
+      concatMap(op => undoer.process(op)),
+      concatMap(ops => Observable.from(ops)),
+      concatMap(op => Promise.all([dbHandler.handle(op), wsHandler.handle(op)]))
     )
     .subscribe()
 
